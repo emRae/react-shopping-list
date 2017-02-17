d8745d38fca74ebe8e11ed5864fa132fbcf7ac7d
Katelyn  Williamson <katew@Katelyns-MacBook-Pro.local>
2017-02-16 19:18:09 -0700

    List Component

---
+++ src/List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class List extends React.Component{
+    render(){
+        return (
+            <ul>
+                {items}
+            </ul>
+        )
+    }
+}
+export default List;
